36c71efa4f73a9ef8f4c9c92c8505e2479b8178f
J0na555 <ethiopediaa@gmail.com>
2026-04-30 15:47:25 +0300

    solved some cf problems

--- contests/a2sv #14/C_Prefix_Max.py
+++ contests/a2sv #14/C_Prefix_Max.py
@@ -4,14 +4,28 @@ for _ in range(t):
     n = int(input())
     nums = list(map(int, input().split()))
 
-    sum = 0
-    max_num = 0
+    # Calculate value of original array
+    def calculate_value(arr):
+        total = 0
+        current_max = 0
+        for x in arr:
+            if x > current_max:
+                current_max = x
+            total += current_max
+        return total
+
+    original_value = calculate_value(nums)
+    
+    # Try swapping the first element with the maximum element
     max_num = max(nums)
-    sum += max_num
-
-
-    for i in range(n):
+    max_index = nums.index(max_num)
+    
+    best_value = original_value
+    
+    if max_index != 0:
+        # Perform swap
+        nums, nums[max_index] = nums[max_index], nums
+        swapped_value = calculate_value(nums)
+        best_value = max(original_value, swapped_value)
         
-
-    print(sum)
-
+    print(best_value)
